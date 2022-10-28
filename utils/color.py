6c9b4e14bf4f5750d399dad828448b037c856d5a
PaperDragon <2678885646@qq.com>
2022-10-28 16:41:50 +0800

    添加命令行颜色

---
+++ utils/color.py
@@ -0,0 +1,14 @@
+# fonts color
+Green = "\033[32m"
+Red = "\033[31m"
+# Yellow="\033[33m"
+GreenBG = "\033[42;37m"
+RedBG = "\033[41;37m"
+YellowBG = "\033[43;37m"
+Font = "\033[0m"
+
+# notification information
+Info = "${Green}[信息]${Font}"
+OK = f"{Green}[OK]{Font}"
+Error = f"{Red}[错误]{Font}"
+Warning = f"{Red}[警告]{Font}"
