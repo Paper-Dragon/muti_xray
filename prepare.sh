ce2cd1fb7ddf06693a3af03531e25d944fbf449c
Paper-Dragon <2678885646@qq.com>
2022-10-28 18:37:47 +0800

    增加环境准备脚本

---
+++ prepare.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+#fonts color
+Green="\033[32m"
+Red="\033[31m"
+#Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+YellowBG="\033[43;37m"
+Font="\033[0m"
+
+#notification information
+Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+Warning="${Red}[警告]${Font}"
+
+
+source '/etc/os-release'
+VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
+
+
+is_root() {
+  if [ 0 == $UID ]; then
+    echo -e "${OK} ${GreenBG} 当前用户是root用户，进入安装流程 ${Font}"
+    sleep 3
+  else
+    echo -e "${Error} ${RedBG} 当前用户不是root用户，请切换到root用户后重新执行脚本 ${Font}"
+    exit 1
+  fi
+}
+
+judge() {
+  if [[ 0 -eq $? ]]; then
+    echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+    sleep 1
+  else
+    echo -e "${Error} ${RedBG} $1 失败${Font}"
+    exit 1
+  fi
+}
+
+check_system() {
+  if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
+    echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
+    INS="yum"
+  elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]]; then
+    echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
+    INS="apt"
+    $INS update
+    ## 添加 Nginx apt源
+  elif [[ "${ID}" == "ubuntu" && $(echo "${VERSION_ID}" | cut -d '.' -f1) -ge 16 ]]; then
+    echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${UBUNTU_CODENAME} ${Font}"
+    INS="apt"
+    rm /var/lib/dpkg/lock
+    dpkg --configure -a
+    rm /var/lib/apt/lists/lock
+    rm /var/cache/apt/archives/lock
+    $INS update
+  else
+    echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font}"
+    exit 1
+  fi
+
+  $INS install dbus -y
+
+  systemctl stop firewalld
+  systemctl disable firewalld
+  echo -e "${OK} ${GreenBG} firewalld 已关闭 ${Font}"
+
+  systemctl stop ufw
+  systemctl disable ufw
+  echo -e "${OK} ${GreenBG} ufw 已关闭 ${Font}"
+}
+
+dependency_install() {
+  ${INS} install wget git lsof -y
+
+  ${INS} -y install bc
+  judge "安装 bc"
+
+  ${INS} -y install unzip
+  judge "安装 unzip"
+
+  ${INS} -y install curl
+  judge "安装 curl"
+
+  ${INS} -y install python36
+  judge "安装 python36"
+
+  mkdir -p /usr/local/bin >/dev/null 2>&1
+}
+
+basic_optimization() {
+  # 最大文件打开数
+  sed -i '/^\*\ *soft\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
+  sed -i '/^\*\ *hard\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
+  echo '* soft nofile 65536' >>/etc/security/limits.conf
+  echo '* hard nofile 65536' >>/etc/security/limits.conf
+
+  # 关闭 Selinux
+  if [[ "${ID}" == "centos" ]]; then
+    sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+    setenforce 0
+  fi
+
+}
+
+
+prepare_process(){
+  is_root
+  check_system
+  dependency_install
+  basic_optimization
+  judge "系统准备"
+  sleep 5
+  echo "请执行python 命令执行下一步 "
+}
+
+list(){
+    case $1 in
+    run)
+        prepare_process
+        ;;
+
+    *)
+        echo -e "请使用 ${RedBG} bash $0 run ${Font} 选项，以安装准备运行环境"
+        exit 1
+        ;;
+    esac
+}
+
+list "$1"
