ba829ed9977d426ef52ede776218f4929e72a317
Paper-Dragon <2678885646@qq.com>
2022-10-30 12:04:27 +0800

    init config and publish 2 web or txt

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import time
 
 from utils.controllerFactory import Xray, get_net_card, is_root
 from utils.color import *
+from utils.publishFactory import Publish, encode_b64
 
+quick_link_list = []
 xray = Xray()
+publish = Publish(config=quick_link_list)
+
 
 
 def uninstall(args):
@@ -19,16 +23,21 @@ def install(args):
 
 
 def config_init(args):
+
     net_card = get_net_card()
+
     print(f" {Info} {Green} 正常获取网卡信息.... {Font}")
     print(f" {Info} {Green} 你的网卡信息是：{net_card} {Font}")
     print(f" {Info} {Green} 正在生成网络黑洞，用于制作ip和域名封禁功能... {Font}")
     xray.init_config()
     print(f" {OK} {GreenBG} {Red} 网络黑洞生成成功... {Font}")
     while True:
-        black_domain = input(f"请输入被封禁的域名{Red}输入END结束{Font}")
-        if black_domain == "END":
+        black_domain = []
+        black_domain_v = input(f"请输入被封禁的域名{Red}输入END结束{Font}")
+        if black_domain_v == "END":
             break
+        black_domain.append(black_domain_v)
+    if black_domain != '':
         xray.insert_black_domain(black_domain)
     top_mode = input("请输入你要制作的协议：【sock5/vmess】")
     if top_mode == "sock5":
@@ -39,24 +48,27 @@ def config_init(args):
         print(f"{Warning} {Red}作者还没写这个模式 {top_mode} 请联系作者 {Green} {author_email} {Font}")
         exit(2)
     for ip in net_card:
-        node_info = []
         print(f"{Info} 正在处理 {ip} {Font}")
         tag = xray.gen_tag(ipaddr=ip)
         port = random.randint(30000, 50000)
-        # print(f"{tag[0]}  {tag[1]}")
         xray.insert_routing_config(tag[0], tag[1])
         xray.insert_outbounds_config(ipaddr=ip, outbound_tag=tag[1])
         time.sleep(1)
-        name = tag[2]
+        name = args.name + '-' + tag[2]
         if top_mode == "sock5":
             user = ''.join(random.sample(string.ascii_letters + string.digits, 16))
             passwd = ''.join(random.sample(string.ascii_letters + string.digits, 16))
-            xray.insert_inbounds_sk5_tcp_config(ipaddr=ip, port=port, inbounds_tag=tag[0], user=user, passwd=passwd, name=name)
-
+            xray.insert_inbounds_sk5_tcp_config(ipaddr=ip, port=port, inbounds_tag=tag[0], user=user, passwd=passwd,
+                                                name=name)
+            b64 = encode_b64(f"{user}:{passwd}")
+            quick_link = f"socks://{b64}@{ip}:{port}#{name}"
+            quick_link_list.append(quick_link)
+    # print(quick_link_list)
     xray.write_2_file()
     print(f"{OK} {Green} 配置生成完毕! {Font}")
     xray.restart()
     print(f"{OK} {Green} 内核重载配置完毕! {Font}")
+    publish.publish_2_web()
     # xray.print_file_config()
 
 
@@ -68,30 +80,37 @@ def status(args):
     xray.status()
 
 
+def show_file_config(args):
+    xray.print_file_config()
+
+
 if __name__ == '__main__':
     if not is_root():
         print(f"{Error} {Red}请使用root运行{Font}")
         exit(1)
 
-    parser = argparse.ArgumentParser(description='Mutilation IP Cluster Server Management Script')
+    parser = argparse.ArgumentParser(description=f'{Red}站群服务器隧道管理脚本{Font}')
     parser.add_argument("--list", '-L', action='store_true', default=False,
-                        help="list all nodes in this Cluster server")
+                        help="列出站群服务器内的所有节点")
     parser.set_defaults(func=list_node)
-    subparsers = parser.add_subparsers(help='choose into sub menu')
+    subparsers = parser.add_subparsers(help='选择进入子菜单功能')
 
-    parser_install = subparsers.add_parser('install', help='Full Install xray')
+    parser_install = subparsers.add_parser('install', help='完整安装Xray【不包含配置】')
     parser_install.set_defaults(func=install)
 
-    parser_config_init = subparsers.add_parser('config_init', help='Init Config')
-    # parser_config_init.add_argument('--name', type=str, help='Prefix name of the generated node')
+    parser_config_init = subparsers.add_parser('config_init', help='进行配置初始化并重载内核设置')
+    parser_config_init.add_argument('--name', type=str, help='节点名称的前缀')
     # parser_config_init.add_argument('--mode', type=str, help='Transport Layer Protocol')
     parser_config_init.set_defaults(func=config_init)
 
-    parser_uninstall = subparsers.add_parser('uninstall', help='Remove From This Computer')
+    parser_uninstall = subparsers.add_parser('uninstall', help='从这个电脑上完全移除站群管理服务')
     parser_uninstall.set_defaults(func=uninstall)
 
-    parser_status = subparsers.add_parser('status', help="show xray status")
+    parser_status = subparsers.add_parser('status', help="查看xray运行状态")
     parser_status.set_defaults(func=status)
+
+    parser_config = subparsers.add_parser('show_config', help="查看文件中的配置")
+    parser_config.set_defaults(func=show_file_config)
 
     # parser_s = subparsers.add_parser('modify', help='Edit the name of a node')
     # parser_s.add_argument('--name', type=str, help='NodeName')
