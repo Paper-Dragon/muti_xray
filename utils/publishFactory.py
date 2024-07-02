28ee5a3ca62608eda158398c2851d1ca5a64e7db
Paper-Dragon <2678885646@qq.com>
2024-07-02 06:41:30 +0000

    重构Python，增强代码健壮性

--- utils/publishFactory.py
+++ utils/publishFactory.py
@@ -1,39 +1,37 @@
 import base64
 import os
 import json
-from utils.color import *
+from .color import *
 
 
 def encode_b64(strings):
-    bb = str(strings).encode()
-    cc = base64.b64encode(bb)
-    strings = str(cc, 'utf-8')
-    return strings
+    """编码一个字符串到base64."""
+    return base64.b64encode(strings.encode()).decode('utf-8')
 
 
 class Publish(object):
-    def __init__(self, config):
+    def __init__(self, config=None):
         """
-        发布一个列表到网页端或者文件内
+        发布一个配置列表到网络服务或者文件内
         :param config: list
         """
-        self.config = config
+        self.config = config if config is not None else []
         self.quick_file = "quick_link.txt"
         self.p_web = "dpaste.com"
 
     def publish_2_web(self):
+        """将当前的配置列表发布到一个网络服务上"""
         self.publish_2_txt()
-        print(f"发布的链接是： {Green}  ")
+        print(f"{Green}发布的链接是： ")
         os.system(f"./common/pastebinit-1.5/pastebinit -i {self.quick_file} -b {self.p_web}")
         print(Font)
 
     def publish_2_txt(self):
-        f = open(self.quick_file, 'a')
-        for i in self.config:
-            f.write(i)
-            f.write("\n")
-        print(f"{OK} {Green} 我们已经帮你记在了 {Red} {self.quick_file} {Green} 文件里 {Font}")
-        f.close()
+        """将当前的配置列表写入到一个文本文件中"""
+        with open(self.quick_file, 'a') as f:
+            for config_item in self.config:
+                f.write(config_item + "\n")
+        print(f"{OK}{Green} 我们已经帮你记在了 {Red}{self.quick_file}{Green} 文件里 {Font}")
 
     def create_vmess_ws_quick_link(self, ps, address, uuid, port, alert_id, path):
         """
@@ -65,7 +63,7 @@ class Publish(object):
         }
         self.create_vmess_code(user_config=user_config)
 
-    def create_vmess_tcp_quick_link(self,ps, address, uuid, port, alert_id):
+    def create_vmess_tcp_quick_link(self, ps, address, uuid, port, alert_id):
         """
         :param ps: 节点名称
         :param address: 节点地址
@@ -94,7 +92,7 @@ class Publish(object):
         }
         self.create_vmess_code(user_config=user_config)
 
-    def create_vmess_code(self,user_config):
+    def create_vmess_code(self, user_config):
         aaa = json.dumps(user_config, indent=4, separators=(',', ': '))
 
         bb = str(aaa).encode()
@@ -105,18 +103,19 @@ class Publish(object):
         # f.write(vmess_link)
         # f.write("\n")
         # f.close()
-        #print("DEBUG quicklink is", vmess_link)
+        # print("DEBUG quicklink is", vmess_link)
+    
+    
+    def __test_base64_encoding(self, strings):
+        encoded = encode_b64(strings)
+        print(encoded)
+        os.system(f"echo {encoded} | base64 -d")
 
 
+def _test_base64_encoding():
+    publish_instance = Publish()
+    publish_instance.__test_base64_encoding(author_email)
 
 
-
-def __test_b64e(strings):
-    b64 = encode_b64(strings)
-    print(b64)
-    os.system(f"echo {b64} | base64 -d")
-
-
-# test
 if __name__ == '__main__':
-    __test_b64e(f"{author_email}")
+    _test_base64_encoding()
