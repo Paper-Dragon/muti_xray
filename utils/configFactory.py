60a3560d119d7c88ec50e407c47c47670a40a85d
Paper-Dragon <2678885646@qq.com>
2024-09-01 02:39:35 +0800

    get_tag update description

--- utils/configFactory.py
+++ utils/configFactory.py
@@ -1,6 +1,8 @@
 import json
 import os
 import time
+from typing import AnyStr
+
 from .color import *
 import models
 
@@ -51,8 +53,14 @@ class Config:
         self.myconfig['inbounds'].append(inbound_dict)
 
     @staticmethod
-    def gen_tag(ipaddr):
-        """生成标签"""
+    def gen_tag(ipaddr: AnyStr = "127.0.0.1") -> list:
+        """
+        生成标签
+
+        :param ipaddr: server ip address
+        :type ipaddr: AnyStr
+        :return list[inbound_tag, outbound_tag, suffix]
+        """
         # 如果ps字段有用，保留；否则考虑是否有必要返回ps
         inbound_tag = f"in-{ipaddr.replace('.', '-')}"
         outbound_tag = f"out-{ipaddr.replace('.', '-')}"
