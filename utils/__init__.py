04a811e8185aaf2deb81f8287c15c231c7c801a3
Paper-Dragon <2678885646@qq.com>
2024-09-01 15:03:28 +0800

    refact use colorterm module

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .color import Warning,Error,OK,Info
+from .configFactory import Config
+from .controllerFactory import Xray, is_root
+from .publishFactory import Publish
+author_email = '2678885646@qq.com'
