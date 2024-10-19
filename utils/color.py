7629653b7f6a757f2eebddfbfa03271ee93858fa
PaperDragon <2678885646@qq.com>
2024-10-19 12:50:04 +0800

    python 3.6, centos7,rhel support, remove termcolor, update pstuil to 6.1.0

--- utils/color.py
+++ utils/color.py
@@ -1,7 +1,20 @@
-from termcolor import colored
+GREEN = '\033[92m'
+BLUE = '\033[94m'
+RED = '\033[91m'
+YELLOW = '\033[93m'
 
-OK = f"{colored(text='[成功]', color='green', on_color='on_black',force_color=True)}"
-Info = f"{colored(text='[信息]',color='blue', on_color='on_black', force_color=True)}"
-Error = f"{colored(text='[错误]', color='red', on_color='on_black', force_color=True)}"
-Warning = f"{colored(text='[警告]', color='yellow', on_color='on_black', force_color=True)}"
+BLACK_BG = '\033[40m'
+GREEN_BG = '\033[42m'
 
+FONT = '\033[0m'
+
+OK = f"{GREEN}{BLACK_BG}[成功]{FONT}"
+Info = f"{BLUE}{BLACK_BG}[信息]{FONT}"
+Error = f"{RED}{BLACK_BG}[错误]{FONT}"
+Warning = f"{YELLOW}{BLACK_BG}[警告]{FONT}"
+
+def _test_color():
+    print(OK)
+    print(Info)
+    print(Error)
+    print(Warning)
